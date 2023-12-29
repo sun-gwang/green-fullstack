787f49ee0381ca6ea327486e4dc6926b1d488dfd
kkj80911 <kkj80911@gmail.com>
2023-12-29 17:19:56 +0900

    add

--- CordingTest/src/stpe1/Main1_10.java
+++ CordingTest/src/stpe1/Main1_10.java
@@ -1,5 +1,21 @@
 package stpe1;
 
+import java.util.Scanner;
+
 public class Main1_10 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a*(b%10));
+		System.out.println(a*((b%100)/10));
+		System.out.println(a*(b/100));
+		System.out.println(a*b);
+		
+	}
 
 }
