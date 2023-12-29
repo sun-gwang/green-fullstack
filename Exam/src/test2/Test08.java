feb77cd0042d41efc7314dc5db44062cc7883940
kkj80911 <kkj80911@gmail.com>
2023-12-29 18:22:34 +0900

    test2 추가

--- Exam/src/test2/Test08.java
+++ Exam/src/test2/Test08.java
@@ -1,1 +1,26 @@
-package test2;public class Test08 {	public static void main(String[] args) {				int result = factorial(3);				System.out.println("3! = " + result);		System.out.println("4! = "+factorial(4));		System.out.println("5! = "+factorial(5));			}		public static int factorial(int n) {		if (n <= 1) {			return 1;		}				return n * factorial(n-1);	}}
+package test2;
+/*
+ * 날짜 : 2023/12/29
+ * 이름 : 김선광
+ * 내용 : 자바 팩토리얼 재귀 메서드 연습문제
+ */
+public class Test08 {
+	public static void main(String[] args) {
+
+		
+		int result = factorial(3);
+		
+		System.out.println("3! = " + result);
+		System.out.println("4! = "+factorial(4));
+		System.out.println("5! = "+factorial(5));		
+	}
+	
+	public static int factorial(int n) {
+
+		if (n <= 1) {
+			return 1;
+		}
+		
+		return n * factorial(n-1);
+	}
+}
