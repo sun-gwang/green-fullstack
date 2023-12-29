1fbe5b2497948d0f007640be403b0aae7fc27398
kkj80911 <kkj80911@gmail.com>
2023-12-29 18:26:48 +0900

    add

--- CordingTest/src/stpe1/Main1_08.java
+++ CordingTest/src/stpe1/Main1_08.java
@@ -1,5 +1,17 @@
 package stpe1;
 
+import java.util.Scanner;
+
 public class Main1_08 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		
+		System.out.println(a-543);
+		
+	}
 
 }
+
