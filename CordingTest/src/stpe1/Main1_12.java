0fe3db66de3d7aba0cb7ddb94edada66fbc58bfb
kkj80911 <kkj80911@gmail.com>
2023-12-29 17:21:28 +0900

    add

---
+++ CordingTest/src/stpe1/Main1_12.java
@@ -0,0 +1,13 @@
+package stpe1;
+
+public class Main1_12 {
+	public static void main(String[] args) {
+		
+		System.out.println("\\    /\\\r\n"
+				         + " )  ( ')\r\n"
+				         + "(  /  )\r\n"
+				         + " \\(__)|");
+		
+	}
+
+}
