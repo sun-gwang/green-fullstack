b9ba79b141c25d91436a81f5088f7443f62e8dbf
kkj89011 <kkj80911@gmail.com>
2024-01-07 15:01:16 +0900

    수정

--- Exam/src/test3/Test09.java
+++ Exam/src/test3/Test09.java
@@ -21,7 +21,6 @@ class Triangle extends Shape{
 
 class Circle extends Shape{
 
-
 	@Override
 	public void draw() {
 		System.out.println("draw Circle!");		
@@ -33,17 +32,16 @@ public class Test09 {
 	public static void main(String[] args) {
 		
 		Test09 here = new Test09();
-		
-		Circle   circle   = new Circle();
+		Circle circle = new Circle();
 		Triangle triangle = new Triangle();
 		
 		here.draw(circle);
 		here.draw(triangle);
-		
-		public void draw (shaped obj) {
-			obj.draw();
-			}
-		
 	}
-
+		
+		public void draw(Shape obj) {
+			obj.draw();
+		}
+		
 }
+
