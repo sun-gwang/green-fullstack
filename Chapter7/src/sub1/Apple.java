020792b2805500343d29074b1fb5ea993eb23bbc
kkj80911 <kkj80911@gmail.com>
2024-01-09 17:52:26 +0900

    Chapter7 추가

---
+++ Chapter7/src/sub1/Apple.java
@@ -0,0 +1,19 @@
+package sub1;
+public class Apple //extends Object
+{
+	private String country;
+	private int price;
+	
+	public Apple(String country, int price) {
+		
+		this.country = country;
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Apple [hashCode ="+hashCode()+",country=" +
+	                   country + ", price=" + price + "]";
+	}
+	
+}
