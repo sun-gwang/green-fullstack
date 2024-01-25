1caed50beb56f9e817c3f4c75cb620b737bc7242
kkj80911 <kkj80911@gmail.com>
2024-01-25 17:19:27 +0900

    미니프로젝트

---
+++ MyShop/src/com/myshop/db/DBHelper.java
@@ -0,0 +1,49 @@
+package com.myshop.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBHelper {
+	
+	// DB정보
+	private final String HOST = "jdbc:mysql://127.0.0.1:3306/shop";
+	private final String USER = "kc5353";
+	private final String PASS = "abcd1234";
+		
+	// DB 자원
+	protected Connection        conn = null;
+	protected Statement         stmt = null;
+	protected PreparedStatement psmt = null;
+	protected ResultSet           rs = null;
+		
+	protected Connection getConnection() throws ClassNotFoundException ,SQLException {
+			
+	Class.forName("com.mysql.cj.jdbc.Driver");
+	return DriverManager.getConnection(HOST, USER, PASS);
+		}
+		
+	protected void Close() throws SQLException {
+			
+			if(rs!=null) {
+				rs.close();
+			}
+			
+			if(stmt!=null) {
+				stmt.close();
+			}
+			
+			if(psmt!=null) {
+				psmt.close();
+			}
+			
+			if(conn!=null) {
+				conn.close();
+			}
+			
+		}
+
+}
---
+++ MyShop/src/com/myshop/db/SQL.java
@@ -0,0 +1,26 @@
+package com.myshop.db;
+
+public class SQL {
+	
+	public static final String INSERT_CUSTOMER = "INSERT INTO `Customer` VALUES (?,?,?,?,NOW()) ";
+	
+	public static final String SELECT_CUSTOMER = "SELECT * FROM `Customer` WHERE `custID`=?";
+	public static final String SELECT_CUSTOMERS = "";
+	public static final String UPDATE_CUSTOMER = "";
+	public static final String DELETE_CUSTOMER = "";
+	
+	public static final String INSERT_ORDER = "INSERT INTO `Order VALUES()";
+	public static final String SELECT_ORDER = "";
+	public static final String SELECT_ORDERS = "SELECT a.*, b.prodNAme, b.price, b.company FROM `Order` as a "
+			                                   + "JOIN `Product` AS b on a.orderProduct = b.prodNO "
+					                           + "WHERE a.OrderID=? "
+							                   + "ORDER BY `orderDate` DESC";       
+	public static final String UPDATE_ORDER = "";
+	public static final String DELETE_ORDER = "";
+
+	public static final String INSERT_PRODUCT = "INSERT INTO `Product`";
+	public static final String SELECT_PRODUCT = "";
+	public static final String SELECT_PRODUCTS = "SELECT * FROM `Product`";
+	public static final String UPDATE_PRODUCT = "";
+	public static final String DELETE_PRODUCT = "";
+}
